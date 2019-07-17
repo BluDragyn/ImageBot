edfb76163fbc31a16fe60c0552ec697fa543a5ab
BluDragyn <dragyn@bludragyn.net>
2019-07-17 05:59:11 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# PRODUCTION BOT CONFIGURATION FILE
+prefix = '!' #Set Command Prefix here
+token = 'YOUR BOTS DISCORD TOKEN'
+library = 'LOCAL IMAGE LIBRARY PATH'
+categories = ['CATEGORIES', #Single words. One word per command/category
+              'GO',
+              'HERE'
+              ]
+
+
+CATEGORY NAME = CHANNEL ID #Repeat for as many categories as you have
+CATEGORY NAME = CHANNEL ID
+CATEGORY NAME = CHANNEL ID
+CATEGORY NAME = CHANNEL ID
+submissions = Selfie Channel ID
